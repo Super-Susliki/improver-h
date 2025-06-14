34b0d6aa1f0180be2698c2dcc11c444caa79fb8f
kostyamospan <kostyamospan@users.noreply.github.com>
2025-06-15 00:11:53 +0300

    fix: hide qr on establishment

--- apps/web/src/components/establishment-details/establishment-details-content.tsx
+++ apps/web/src/components/establishment-details/establishment-details-content.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import { SendTipCard } from "../pages/home/SendTipCard";
 import { QRCodeIcon } from "../svg";
@@ -7,6 +7,7 @@ import { Button } from "../ui/button";
 import { ActionButtons } from "./action-buttons";
 import { QRScannerModal } from "./qr-scanner-modal";
 import { SocialMedia } from "./social-media";
+import { useMerchantStores } from "@/lib/api";
 
 interface Props {
   storeId: string | undefined;
@@ -15,18 +16,26 @@ interface Props {
 export const EstablishmentDetailsContent = ({ storeId }: Props) => {
   const [isQRScannerOpen, setIsQRScannerOpen] = useState(false);
 
+  const { data: stores } = useMerchantStores();
+
+  const store = useMemo(() => {
+    return stores?.find((store) => store.id === storeId);
+  }, [stores, storeId]);
+
   return (
     <div className="flex flex-col gap-5">
       <div className="flex flex-col">
-        <Button
-          className="flex gap-1 items-center"
-          onClick={() => {
-            setIsQRScannerOpen(true);
-          }}
-        >
-          <QRCodeIcon className="size-[26px]" color="white" />
-          Scan QR
-        </Button>
+        {store && (
+          <Button
+            className="flex gap-1 items-center"
+            onClick={() => {
+              setIsQRScannerOpen(true);
+            }}
+          >
+            <QRCodeIcon className="size-[26px]" color="white" />
+            Scan QR
+          </Button>
+        )}
         <SendTipCard />
       </div>
       <ActionButtons />
