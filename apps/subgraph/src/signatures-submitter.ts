c48e85cb0ab894915837e67896b3bbf3bf5364db
kostyamospan <kostyamospan@users.noreply.github.com>
2025-06-14 11:41:13 +0300

    feat: subgraph

---
+++ apps/subgraph/src/signatures-submitter.ts
@@ -0,0 +1,18 @@
+import { SignatureSubmitted as SignatureSubmittedEvent } from "../generated/SignaturesSubmitter/SignaturesSubmitter";
+import { SignatureSubmitted } from "../generated/schema";
+
+export function handleSignatureSubmitted(event: SignatureSubmittedEvent): void {
+  let entity = new SignatureSubmitted(
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  );
+  entity.merchant = event.params.merchant;
+  entity.user = event.params.user;
+  entity.storeId = event.params.storeId;
+  entity.signature = event.params.signature;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
