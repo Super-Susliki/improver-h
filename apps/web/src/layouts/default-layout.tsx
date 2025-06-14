fc822b07ab1c5240ef70c3d7d1c4dfef1d15b427
MrJeleika <den932005@gmail.com>
2025-06-14 17:04:14 +0300

    hide bottom tabs on login

--- apps/web/src/layouts/default-layout.tsx
+++ apps/web/src/layouts/default-layout.tsx
@@ -1,12 +1,20 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 
 import { BottomTabs } from "@/components/bottom-tabs/bottom-tabs";
+import { routes } from "@/lib/router";
+import { cn } from "@/lib/utils";
 
 const DefaultLayout = () => {
+  const { pathname } = useLocation();
+  const isLogin = pathname === routes.login;
   return (
-    <div className="relative flex min-h-screen pb-[58px] max-w-[480px] flex-col px-5 mx-auto items-center">
+    <div
+      className={cn(
+        "relative flex min-h-screen pb-[58px] max-w-[480px] flex-col px-5 mx-auto items-center"
+      )}
+    >
       <Outlet />
-      <BottomTabs />
+      {!isLogin && <BottomTabs />}
     </div>
   );
 };
